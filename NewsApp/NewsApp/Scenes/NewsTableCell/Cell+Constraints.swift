75cb2543d05175412ab638b6ec1824a41916d5ec
omer <omerbzblt613@gmail.com>
2022-07-24 18:30:40 +0300

    table view cell init

---
+++ NewsApp/NewsApp/Scenes/NewsTableCell/Cell+Constraints.swift
@@ -0,0 +1,8 @@
+//
+//  Cell+Constraints.swift
+//  NewsApp
+//
+//  Created by omer faruk bozbulut on 24.07.2022.
+//
+
+import Foundation
---
+++ NewsApp/NewsApp/Scenes/NewsTableCell/NewsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  NewsTableViewCell.swift
+//  NewsApp
+//
+//  Created by omer faruk bozbulut on 24.07.2022.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
