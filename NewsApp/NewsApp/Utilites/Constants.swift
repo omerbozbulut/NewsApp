a06096f1af377b1b7528c330c3c4e6298b1a05ca
omer <omerbzblt613@gmail.com>
2022-07-24 18:26:26 +0300

    constants

---
+++ NewsApp/NewsApp/Utilites/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  NewsApp
+//
+//  Created by omer faruk bozbulut on 23.07.2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let tabBarImages = ["newspaper","bookmark.circle"]
+    static let newsTableViewIdentifier = "news"
+    
+    struct Fonts {
+            static let TrebuchetMSBold = "TrebuchetMS-Bold"
+            static let helvetica = "Helvetica"
+            static let helveticaBOLD = "Helvetica-Bold"
+            static let helveticaMedium = "HelveticaNeue-Medium"
+    }
+
+    struct SymbolNames {
+           static let starSymbolName = "star.fill"
+           static let bookmarkSymbolName = "bookmark"
+           static let bookmarkFillSymbolName = "bookmark.fill"
+           static let chevronBackward = "chevron.backward"
+       }
+}
