17c47147117aeb4e7fbedfa6518765cdd9eda667
omer <omerbzblt613@gmail.com>
2022-07-24 19:43:14 +0300

    models type changed to optional

--- NewsApp/NewsApp/Models/News.swift
+++ NewsApp/NewsApp/Models/News.swift
@@ -13,11 +13,11 @@ struct BaseResponse: Codable {
 }
 
 struct Article: Codable {
-    let author: String
-    let title: String
-    let description: String
-    let url: String
-    let urlToImage: String
-    let publishedAt: String
-    let content: String
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
 }
