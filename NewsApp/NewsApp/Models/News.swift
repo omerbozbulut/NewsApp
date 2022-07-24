5035a10f0dd9e05606be527466cb2c8ee16a6229
omer <omerbzblt613@gmail.com>
2022-07-24 21:48:03 +0300

    Update models

--- NewsApp/NewsApp/Models/News.swift
+++ NewsApp/NewsApp/Models/News.swift
@@ -4,7 +4,7 @@
 //
 //  Created by omer faruk bozbulut on 24.07.2022.
 //
-
+/*
 import Foundation
 
 struct BaseResponse: Codable {
@@ -20,4 +20,38 @@ struct Article: Codable {
     let urlToImage: String?
     let publishedAt: String?
     let content: String?
+}
+ */
+
+import Foundation
+
+// MARK: - BaseResponse
+struct BaseResponse: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source?
+    let author: String?
+    let title: String?
+    let articleDescription: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String
 }
