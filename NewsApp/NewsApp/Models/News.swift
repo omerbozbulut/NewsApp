1e0ec0cb48258113d6356493d7210eb14a558fc4
omer <omerbzblt613@gmail.com>
2022-07-25 00:04:39 +0300

    update news model

--- NewsApp/NewsApp/Models/News.swift
+++ NewsApp/NewsApp/Models/News.swift
@@ -4,24 +4,6 @@
 //
 //  Created by omer faruk bozbulut on 24.07.2022.
 //
-/*
-import Foundation
-
-struct BaseResponse: Codable {
-    let status: String
-    let articles: [Article]
-}
-
-struct Article: Codable {
-    let author: String?
-    let title: String?
-    let description: String?
-    let url: String?
-    let urlToImage: String?
-    let publishedAt: String?
-    let content: String?
-}
- */
 
 import Foundation
 
