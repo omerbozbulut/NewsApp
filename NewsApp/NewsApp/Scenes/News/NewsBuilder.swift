b76de2ccdd85cd154e156540ad0b749f35ceeb90
omer <omerbzblt613@gmail.com>
2022-07-24 23:59:47 +0300

    News page builder added

---
+++ NewsApp/NewsApp/Scenes/News/NewsBuilder.swift
@@ -0,0 +1,17 @@
+//
+//  NewsBuilder.swift
+//  NewsApp
+//
+//  Created by omer faruk bozbulut on 24.07.2022.
+//
+
+import UIKit
+
+final class NewsBuilder {
+    static func build() -> UIViewController {
+        let service = WebService()
+        let viewModel: NewsViewModel = NewsViewModel(service)
+        let vc = NewsViewController(viewModel)
+        return vc
+    }
+}
