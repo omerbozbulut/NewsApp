13852d6708d9d23e00c663a8a59e544e5cb63e8c
omer <omerbzblt613@gmail.com>
2022-07-24 18:26:13 +0300

    notification permission and design added

--- NewsApp/NewsApp/Scenes/Splash/SplashViewController.swift
+++ NewsApp/NewsApp/Scenes/Splash/SplashViewController.swift
@@ -6,24 +6,90 @@
 //
 
 import UIKit
+import SnapKit
 
 class SplashViewController: UIViewController {
 
+    let customView: UIView = {
+        let view = UIView()
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 10
+        view.backgroundColor = .red
+        return view
+    }()
+
+    let titleLabel: UILabel = {
+        let title = UILabel()
+        title.text = "News App"
+        title.textColor = .white
+        title.font = UIFont(name: Constants.Fonts.helveticaBOLD, size: 30)
+        return title
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
 
         configure()
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        tabBarConfigure()
+        super.viewDidAppear(animated)
+
+        registerForRemoteNotification()
     }
 
     func configure(){
+        view.backgroundColor = .white
 
+        view.addSubview(customView)
+        customView.addSubview(titleLabel)
+
+        makeCustomViewConstraints()
+        makeTitleLabelConstraints()
     }
 
+
+//MARK: - Constraints
+    func makeCustomViewConstraints() {
+        customView.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.centerY.equalTo(view.snp.centerY)
+            make.width.equalTo(200)
+            make.height.equalTo(100)
+        }
+    }
+
+    func makeTitleLabelConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(customView.snp.centerX)
+            make.centerY.equalTo(customView.snp.centerY)
+        }
+    }
+
+
+//MARK: - Notification permission
+    func registerForRemoteNotification() {
+        if #available(iOS 10.0, *) {
+            let center  = UNUserNotificationCenter.current()
+
+            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
+                if error == nil{
+                    DispatchQueue.main.async {
+                        UIApplication.shared.registerForRemoteNotifications()
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                        self.tabBarConfigure()
+                    }
+                }
+            }
+        }
+        else {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+    }
+
+//MARK: - Tab bar
     func tabBarConfigure(){
         let tabBarVC = UITabBarController()
 
@@ -43,6 +109,7 @@ class SplashViewController: UIViewController {
 
         tabBarVC.modalPresentationStyle = .fullScreen
         tabBarVC.tabBar.tintColor = .black
+        tabBarVC.tabBar.barTintColor = .white
         self.view.window?.rootViewController = tabBarVC
     }
 }
