ed533347c56fb3ce157e977ec89ac2368e052a02
omer <omerbzblt613@gmail.com>
2022-07-25 23:16:54 +0300

    time interval extension

---
+++ NewsApp/NewsApp/Utilites/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  NewsApp
+//
+//  Created by omer faruk bozbulut on 25.07.2022.
+//
+
+import Foundation
+
+extension TimeInterval{
+
+        func stringFromTimeInterval() -> String {
+            let time = NSInteger(self)
+            let hours = (time / 3600) * -1
+
+            return String(format: "%2d hours ago",hours)
+        }
+    }
