bbfce6fe33f852c946a9ba9ab4705dfbf35df239
omer <omerbzblt613@gmail.com>
2022-08-04 22:06:58 +0300

    Favorite view controller design

--- NewsApp/NewsApp/Scenes/Favorite/FavoriteNewsViewController.swift
+++ NewsApp/NewsApp/Scenes/Favorite/FavoriteNewsViewController.swift
@@ -7,11 +7,60 @@
 
 import UIKit
 
-class FavoriteNewsViewController: UIViewController {
+final class FavoriteNewsViewController: UIViewController {
+
+    let titleLabel: UILabel = {
+        let title = UILabel()
+        title.text = NSLocalizedString("Favorite news", comment: "")
+        title.font = UIFont(name: Constants.Fonts.TrebuchetMSBold, size: 33)
+        return title
+    }()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 144
+        tableView.separatorColor = .white
+        tableView.register(FavoriteTableCell.self, forCellReuseIdentifier: Constants.favoriteTableViewIdentifier)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemIndigo
+
+        view.backgroundColor = .white
+        configure()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel.updateFavoriteList()
+        updateData()
+    }
+
+    let viewModel: FavoriteViewModel
+
+    init(_ viewModel: FavoriteViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(){
+        view.addSubview(tableView)
+        view.addSubview(titleLabel)
+
+        configureConstraints()
+    }
+
+    func updateData() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
 
 }
